a4ec857b05f960f57517da8d011eabe92b64a436
Hemant <nimje.hemant@gmail.com>
2017-02-15 21:43:03 -0800

    Add functionality to order multiple tacos (increase order quantity / decrease order quantity) e.g. (1,2,3,...). Add functionality to choose maximum two filings per taco.

--- app/src/main/java/edu/csulb/android/tacopronto/MainActivity.java
+++ app/src/main/java/edu/csulb/android/tacopronto/MainActivity.java
@@ -2,17 +2,31 @@ package edu.csulb.android.tacopronto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextPaint;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+import static android.os.Build.VERSION_CODES.M;
+
 public class MainActivity extends AppCompatActivity {
+
+    private int quantity = 1;
+    private int fillingCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
 
         /* Find the custom_list_view_fillings and add content to it*/
         /*ListView listViewFillings = (ListView) findViewById(R.id.custom_list_view_fillings);
@@ -45,5 +59,134 @@ public class MainActivity extends AppCompatActivity {
         foodBeverage.add(new Food("Margarita", "2.40"));
         foodBeverage.add(new Food("Tequila", "2.75"));
         listViewBeverage.setAdapter(new CustomAdapter(this, R.layout.custom_row, foodBeverage));*/
+
     }
+
+    public void init() {
+        RadioButton radioButtonLarge = (RadioButton) findViewById(R.id.radio_button_large);
+        RadioButton radioButtonMedium = (RadioButton) findViewById(R.id.radio_button_medium);
+        RadioButton radioButtonCorn = (RadioButton) findViewById(R.id.radio_button_corn);
+        RadioButton radioButtonFlour = (RadioButton) findViewById(R.id.radio_button_flour);
+
+
+        CheckBox checkBoxCheese = (CheckBox) findViewById(R.id.checkbox_cheese);
+        CheckBox checkBoxRice = (CheckBox) findViewById(R.id.checkbox_rice);
+        CheckBox checkBoxBeans = (CheckBox) findViewById(R.id.checkbox_beans);
+        CheckBox checkBoxPicoDeGallo = (CheckBox) findViewById(R.id.checkbox_pico_de_gallo);
+        CheckBox checkBoxGuacamole = (CheckBox) findViewById(R.id.checkbox_guacamole);
+        CheckBox checkBoxLBT = (CheckBox) findViewById(R.id.checkbox_lbt);
+        CheckBox checkBoxSoda = (CheckBox) findViewById(R.id.checkbox_soda);
+        CheckBox checkBoxCerveza = (CheckBox) findViewById(R.id.checkbox_cerveza);
+        CheckBox checkBoxMargarita = (CheckBox) findViewById(R.id.checkbox_margarita);
+        CheckBox checkBoxTequila = (CheckBox) findViewById(R.id.checkbox_tequila);
+    }
+
+
+    /* Choose at least one fillings and not more than two */
+    public void chooseFillings(View view) {
+        CheckBox checkBoxBeef = (CheckBox) findViewById(R.id.checkbox_beef);
+        boolean hasBeef = checkBoxBeef.isChecked();
+
+        CheckBox checkBoxChicken = (CheckBox) findViewById(R.id.checkbox_chicken);
+        boolean hasChicken = checkBoxChicken.isChecked();
+
+        CheckBox checkBoxWhiteFish = (CheckBox) findViewById(R.id.checkbox_white_fish);
+        boolean hasWhiteFish = checkBoxWhiteFish.isChecked();
+
+        CheckBox checkBoxSeaFood = (CheckBox) findViewById(R.id.checkbox_sea_food);
+        boolean hasSeaFood = checkBoxSeaFood.isChecked();
+
+        switch (view.getId()) {
+            case R.id.checkbox_beef:
+                if (hasBeef) {
+                    fillingCount += 1;
+                } else {
+                    fillingCount -= 1;
+                }
+                break;
+            case R.id.checkbox_chicken:
+                if (hasChicken) {
+                    fillingCount += 1;
+                } else {
+                    fillingCount -= 1;
+                }
+                break;
+            case R.id.checkbox_white_fish:
+                if (hasWhiteFish) {
+                    fillingCount += 1;
+                } else {
+                    fillingCount -= 1;
+                }
+                break;
+            case R.id.checkbox_sea_food:
+                if (hasSeaFood) {
+                    fillingCount += 1;
+                } else {
+                    fillingCount -= 1;
+                }
+                break;
+        }
+
+
+        if (fillingCount < 2) {
+            checkBoxBeef.setEnabled(true);
+            checkBoxChicken.setEnabled(true);
+            checkBoxWhiteFish.setEnabled(true);
+            checkBoxSeaFood.setEnabled(true);
+        } else {
+            if (!hasBeef) {
+                checkBoxBeef.setEnabled(false);
+            } else {
+                checkBoxBeef.setEnabled(true);
+            }
+            if (!hasChicken) {
+                checkBoxChicken.setEnabled(false);
+            } else {
+                checkBoxChicken.setEnabled(true);
+            }
+            if (!hasWhiteFish) {
+                checkBoxWhiteFish.setEnabled(false);
+            } else {
+                checkBoxWhiteFish.setEnabled(true);
+            }
+            if (!hasSeaFood) {
+                checkBoxSeaFood.setEnabled(false);
+            } else {
+                checkBoxSeaFood.setEnabled(true);
+            }
+        }
+
+        if (fillingCount == 2) {
+            Toast.makeText(this, "You can select only two fillings", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /* Increase the number of orders to be placed */
+    public void increment(View view) {
+        if (quantity == 9) {
+            Toast.makeText(this, "Can't order more than 9 orders at a time.", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            quantity = quantity + 1;
+
+        }
+        display(quantity);
+    }
+
+    /* Decrease the number of orders to be placed */
+    public void decrement(View view) {
+        if (quantity > 1) {
+            quantity = quantity - 1;
+        } else {
+            Toast.makeText(this, "Minimum 1 order needs to be placed", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        display(quantity);
+    }
+
+    private void display(int quantity) {
+        TextView orderQuantity = (TextView) findViewById(R.id.text_view_order_quantity);
+        orderQuantity.setText("" + quantity);
+    }
+
 }
