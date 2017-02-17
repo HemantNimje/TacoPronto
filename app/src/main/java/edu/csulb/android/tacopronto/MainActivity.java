14115c63dcb9e6f14d224c01ecf7f11195c52631
Hemant <nimje.hemant@gmail.com>
2017-02-16 17:39:45 -0800

    Add functionality to choose maximum four toppings. Add functionality to display cost

--- app/src/main/java/edu/csulb/android/tacopronto/MainActivity.java
+++ app/src/main/java/edu/csulb/android/tacopronto/MainActivity.java
@@ -2,25 +2,27 @@ package edu.csulb.android.tacopronto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextPaint;
 import android.view.View;
 import android.widget.CheckBox;
-import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.math.BigDecimal;
 
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
-import static android.os.Build.VERSION_CODES.M;
+import static edu.csulb.android.tacopronto.R.string.price;
 
 public class MainActivity extends AppCompatActivity {
 
     private int quantity = 1;
     private int fillingCount = 0;
     private int toppingCount = 0;
+    private BigDecimal bPrice = new BigDecimal(0);
+    private BigDecimal finalPrice = new BigDecimal(0);
+    private BigDecimal changedPrice;
+
+
+    RadioButton radioButtonLarge, radioButtonMedium, radioButtonCorn, radioButtonFlour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,19 +66,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void init() {
-        RadioButton radioButtonLarge = (RadioButton) findViewById(R.id.radio_button_large);
-        RadioButton radioButtonMedium = (RadioButton) findViewById(R.id.radio_button_medium);
-        RadioButton radioButtonCorn = (RadioButton) findViewById(R.id.radio_button_corn);
-        RadioButton radioButtonFlour = (RadioButton) findViewById(R.id.radio_button_flour);
+        radioButtonLarge = (RadioButton) findViewById(R.id.radio_button_large);
+        radioButtonMedium = (RadioButton) findViewById(R.id.radio_button_medium);
 
-        CheckBox checkBoxSoda = (CheckBox) findViewById(R.id.checkbox_soda);
-        CheckBox checkBoxCerveza = (CheckBox) findViewById(R.id.checkbox_cerveza);
-        CheckBox checkBoxMargarita = (CheckBox) findViewById(R.id.checkbox_margarita);
-        CheckBox checkBoxTequila = (CheckBox) findViewById(R.id.checkbox_tequila);
     }
 
-
-    /* Choose at least one fillings and not more than two */
+    /* Choose maximum two fillings */
     public void chooseFillings(View view) {
         CheckBox checkBoxBeef = (CheckBox) findViewById(R.id.checkbox_beef);
         boolean hasBeef = checkBoxBeef.isChecked();
@@ -94,33 +89,40 @@ public class MainActivity extends AppCompatActivity {
             case R.id.checkbox_beef:
                 if (hasBeef) {
                     fillingCount += 1;
+                    bPrice = bPrice.add(BigDecimal.valueOf(2.70));
                 } else {
                     fillingCount -= 1;
+                    bPrice = bPrice.subtract(BigDecimal.valueOf(2.70));
                 }
                 break;
             case R.id.checkbox_chicken:
                 if (hasChicken) {
                     fillingCount += 1;
+                    bPrice = bPrice.add(BigDecimal.valueOf(2.40));
                 } else {
                     fillingCount -= 1;
+                    bPrice = bPrice.subtract(BigDecimal.valueOf(2.40));
                 }
                 break;
             case R.id.checkbox_white_fish:
                 if (hasWhiteFish) {
                     fillingCount += 1;
+                    bPrice = bPrice.add(BigDecimal.valueOf(2.45));
                 } else {
                     fillingCount -= 1;
+                    bPrice = bPrice.subtract(BigDecimal.valueOf(2.45));
                 }
                 break;
             case R.id.checkbox_sea_food:
                 if (hasSeaFood) {
                     fillingCount += 1;
+                    bPrice = bPrice.add(BigDecimal.valueOf(2.60));
                 } else {
                     fillingCount -= 1;
+                    bPrice = bPrice.subtract(BigDecimal.valueOf(2.60));
                 }
                 break;
         }
-
 
         if (fillingCount < 2) {
             checkBoxBeef.setEnabled(true);
@@ -153,36 +155,10 @@ public class MainActivity extends AppCompatActivity {
         if (fillingCount == 2) {
             Toast.makeText(this, "You can select only two fillings", Toast.LENGTH_SHORT).show();
         }
+        calculatePrice();
     }
 
-    /* Increase the number of orders to be placed */
-    public void increment(View view) {
-        if (quantity == 9) {
-            Toast.makeText(this, "Can't order more than 9 orders at a time.", Toast.LENGTH_SHORT).show();
-            return;
-        } else {
-            quantity = quantity + 1;
-
-        }
-        display(quantity);
-    }
-
-    /* Decrease the number of orders to be placed */
-    public void decrement(View view) {
-        if (quantity > 1) {
-            quantity = quantity - 1;
-        } else {
-            Toast.makeText(this, "Minimum 1 order needs to be placed", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        display(quantity);
-    }
-
-    private void display(int quantity) {
-        TextView orderQuantity = (TextView) findViewById(R.id.text_view_order_quantity);
-        orderQuantity.setText("" + quantity);
-    }
-
+    /* Choose maximum four toppings */
     public void chooseToppings(View view) {
         CheckBox checkBoxCheese = (CheckBox) findViewById(R.id.checkbox_cheese);
         boolean hasCheese = checkBoxCheese.isChecked();
@@ -206,43 +182,56 @@ public class MainActivity extends AppCompatActivity {
             case R.id.checkbox_cheese:
                 if (hasCheese) {
                     toppingCount += 1;
+                    bPrice = bPrice.add(BigDecimal.valueOf(0.50));
+
                 } else {
                     toppingCount -= 1;
+                    bPrice = bPrice.subtract(BigDecimal.valueOf(0.50));
                 }
                 break;
             case R.id.checkbox_rice:
                 if (hasRice) {
                     toppingCount += 1;
+                    bPrice = bPrice.add(BigDecimal.valueOf(1.00));
                 } else {
                     toppingCount -= 1;
+                    bPrice = bPrice.subtract(BigDecimal.valueOf(1.00));
                 }
                 break;
             case R.id.checkbox_beans:
                 if (hasBeans) {
                     toppingCount += 1;
+                    bPrice = bPrice.add(BigDecimal.valueOf(0.65));
                 } else {
                     toppingCount -= 1;
+                    bPrice = bPrice.subtract(BigDecimal.valueOf(0.65));
                 }
                 break;
             case R.id.checkbox_pico_de_gallo:
                 if (hasPicoDeGallo) {
                     toppingCount += 1;
+                    bPrice = bPrice.add(BigDecimal.valueOf(0.50));
                 } else {
                     toppingCount -= 1;
+                    bPrice = bPrice.subtract(BigDecimal.valueOf(0.50));
                 }
                 break;
             case R.id.checkbox_guacamole:
                 if (hasGaucamole) {
                     toppingCount += 1;
+                    bPrice = bPrice.add(BigDecimal.valueOf(1.25));
                 } else {
                     toppingCount -= 1;
+                    bPrice = bPrice.subtract(BigDecimal.valueOf(1.25));
                 }
                 break;
             case R.id.checkbox_lbt:
                 if (hasLBT) {
                     toppingCount += 1;
+                    bPrice = bPrice.add(BigDecimal.valueOf(1.50));
                 } else {
                     toppingCount -= 1;
+                    bPrice = bPrice.subtract(BigDecimal.valueOf(1.50));
                 }
                 break;
         }
@@ -290,5 +279,104 @@ public class MainActivity extends AppCompatActivity {
         if (toppingCount == 4) {
             Toast.makeText(this, "You can select only four toppings", Toast.LENGTH_SHORT).show();
         }
+        calculatePrice();
     }
+
+    /* Choose beverages */
+    public void chooseBeverages(View view) {
+
+        CheckBox checkBoxSoda = (CheckBox) findViewById(R.id.checkbox_soda);
+        boolean hasSoda = checkBoxSoda.isChecked();
+
+        CheckBox checkBoxCerveza = (CheckBox) findViewById(R.id.checkbox_cerveza);
+        boolean hasCerveza = checkBoxCerveza.isChecked();
+
+        CheckBox checkBoxMargarita = (CheckBox) findViewById(R.id.checkbox_margarita);
+        boolean hasMargarita = checkBoxMargarita.isChecked();
+
+        CheckBox checkBoxTequila = (CheckBox) findViewById(R.id.checkbox_tequila);
+        boolean hasTequila = checkBoxTequila.isChecked();
+
+        switch (view.getId()) {
+            case R.id.checkbox_soda:
+                if (hasSoda) {
+                    bPrice = bPrice.add(BigDecimal.valueOf(1.45));
+                } else {
+                    bPrice = bPrice.subtract(BigDecimal.valueOf(1.45));
+                }
+                break;
+            case R.id.checkbox_cerveza:
+                if (hasCerveza) {
+                    bPrice = bPrice.add(BigDecimal.valueOf(1.75));
+                } else {
+                    bPrice = bPrice.subtract(BigDecimal.valueOf(1.75));
+                }
+                break;
+            case R.id.checkbox_margarita:
+                if (hasMargarita) {
+                    bPrice = bPrice.add(BigDecimal.valueOf(2.00));
+                } else {
+                    bPrice = bPrice.subtract(BigDecimal.valueOf(2.00));
+                }
+                break;
+            case R.id.checkbox_tequila:
+                if (hasTequila) {
+                    bPrice = bPrice.add(BigDecimal.valueOf(3.45));
+                } else {
+                    bPrice = bPrice.subtract(BigDecimal.valueOf(3.45));
+                }
+                break;
+        }
+        calculatePrice();
+    }
+
+    /* Increase the number of orders to be placed */
+    public void increment(View view) {
+        if (quantity == 9) {
+            Toast.makeText(this, "Can't order more than 9 orders at a time.", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            quantity = quantity + 1;
+
+        }
+        displayQuantity(quantity);
+    }
+
+    /* Decrease the number of orders to be placed */
+    public void decrement(View view) {
+        if (quantity > 1) {
+            quantity = quantity - 1;
+        } else {
+            Toast.makeText(this, "Minimum 1 order needs to be placed", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        displayQuantity(quantity);
+    }
+
+    /* Display the number of tacos */
+    private void displayQuantity(int quantity) {
+        TextView orderQuantity = (TextView) findViewById(R.id.text_view_order_quantity);
+        orderQuantity.setText("" + quantity);
+    }
+
+    /* Display the total cost */
+    private void displayCost() {
+        TextView textViewTotalCost = (TextView) findViewById(R.id.text_view_total_cost);
+
+        textViewTotalCost.setText("$" + finalPrice);
+        //Toast.makeText(this,"Total Cost:"+bPrice,Toast.LENGTH_SHORT).show();
+    }
+
+    private void calculatePrice() {
+        if (radioButtonLarge.isChecked()) {
+            changedPrice = new BigDecimal(quantity);
+            changedPrice = changedPrice.multiply(BigDecimal.valueOf(1.2));
+            finalPrice = bPrice.multiply(changedPrice);
+        }
+        if (radioButtonMedium.isChecked()) {
+            finalPrice = bPrice.multiply(new BigDecimal(quantity));
+        }
+        displayCost();
+    }
+
 }
